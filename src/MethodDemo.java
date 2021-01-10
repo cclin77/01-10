96f58f40d8991a88090dec4fecb09a66c7f8acce
cc <icecreamc@qq.com>
2021-01-10 09:41:29 +0800

    test

--- src/MethodDemo.java
+++ src/MethodDemo.java
@@ -1,2 +1,12 @@
+
 public class MethodDemo {
+    public static void main(String args[]){
+        int a=3;
+        m1(a);
+        System.out.println("AAAAA:"+a);
+    }
+    public static void m1(int a){
+        a=5;
+        System.out.println("CCCCC:"+a);
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,12 @@
 
 public class Test {
     public static void main(String args[]){
-        System.out.println("hello");
-
+        int a=3;
+        m1(a);
+        System.out.println("AAAAA:"+a);
+    }
+    public static void m1(int a){
+        a=5;
+        System.out.println("CCCCC:"+a);
     }
 }
